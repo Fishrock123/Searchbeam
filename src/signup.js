cadff255394937ca3a37bd7beb908b6b9c41dd1c
Fishrock123 <fishrock123@rocketmail.com>
2014-01-03 11:17:03 -0500

    Applied semicolon style changes to client src js files.

--- src/signup.js
+++ src/signup.js
@@ -3,51 +3,51 @@ $(document).ready(function() {
       user: {timer: 0, valid: false, check: checkUser, ajax: undefined},
       pass: {timer: 0, valid: false, check: checkPass, ajax: undefined},
       conf: {timer: 0, valid: false, check: confPass}
-    }, valid = false, ajax = undefined;
-  $('#s_user').on('input', fields.user, setCheckerTimer);
-  $('#s_pass').on('input', fields.pass, setCheckerTimer);
-  $('#s_pass_conf').on('input', fields.conf, setCheckerTimer);
+    }, valid = false, ajax = undefined
+  $('#s_user').on('input', fields.user, setCheckerTimer)
+  $('#s_pass').on('input', fields.pass, setCheckerTimer)
+  $('#s_pass_conf').on('input', fields.conf, setCheckerTimer)
 
-  $("#signup").submit(verifySubmit);
+  $("#signup").submit(verifySubmit)
 
   function verifySubmit(ev) {
-    var that = this;
+    var that = this
     if (valid) {
-      if (ajax) ajax.abort();
+      if (ajax) ajax.abort()
       ajax = $.ajax({
         type: "POST",
         url: 'register',
         data: $(that).serialize(),
         dataType: "json",
         error: function(jqXHR, textStatus, errorThrown) {
-          $('#s_submit').parent().children(".icon").addClass('err').removeClass("hidden green");
+          $('#s_submit').parent().children(".icon").addClass('err').removeClass("hidden green")
         },
         success: function(data) {
           if (data.user) {
             if (data.user.valid) {
-              fields.user.valid = data.user.valid;
-              $('#s_user').parent().children(".icon").removeClass("err").addClass("green").html("&#xF007;");
+              fields.user.valid = data.user.valid
+              $('#s_user').parent().children(".icon").removeClass("err").addClass("green").html("&#xF007;")
             } else {
-              if (data.user.err) $('#err_msg').text(data.user.err);
-              $('#s_user').parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;");
+              if (data.user.err) $('#err_msg').text(data.user.err)
+              $('#s_user').parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;")
             }
           }
           if (data.pass) {
             if (data.pass.valid) {
-              fields.pass.valid = data.pass.valid;
-              $('#s_pass').parent().children(".icon").removeClass("err").addClass("green").html("&#xF084;");
+              fields.pass.valid = data.pass.valid
+              $('#s_pass').parent().children(".icon").removeClass("err").addClass("green").html("&#xF084;")
             } else {
-              if (data.pass.err) $('#err_msg').text(data.pass.err);
-              $('#s_pass').parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;");
+              if (data.pass.err) $('#err_msg').text(data.pass.err)
+              $('#s_pass').parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;")
             }
           }
-          checkValidated();
+          checkValidated()
           if (data.err) {
-            if (data.err) $('#err_msg').text(data.err);
-            $('#s_submit').parent().children(".icon").addClass('err').removeClass("hidden green");
+            if (data.err) $('#err_msg').text(data.err)
+            $('#s_submit').parent().children(".icon").addClass('err').removeClass("hidden green")
           } else if (data.registered) {
-            $('#s_submit').parent().children(".icon").removeClass("err hidden").addClass("green").html("&#xF00C;");
-            $('#s_submit').addClass('disabled greyed');
+            $('#s_submit').parent().children(".icon").removeClass("err hidden").addClass("green").html("&#xF00C;")
+            $('#s_submit').addClass('disabled greyed')
 
             $.ajax({
               type: "POST",
@@ -55,65 +55,65 @@ $(document).ready(function() {
               data: $(that).serialize(),
               dataType: "json",
               error: function(jqXHR, textStatus, errorThrown) {
-                $('#err_msg').text(textStatus);
-                console.log(textStatus);
-                console.log(jqXHR);
+                $('#err_msg').text(textStatus)
+                console.log(textStatus)
+                console.log(jqXHR)
               },
               success: function(data) {
                 if (data.error) {
-                  $("#auth-err").show().text(data.error);
-                  $('#err_msg').text(data.error);
+                  $("#auth-err").show().text(data.error)
+                  $('#err_msg').text(data.error)
                 } else if (data.success) {
-                  $("#auth-success").show().text(data.success);
-                  $("#auth").html(data.html);
-                  $('.login').addClass('hidden');
-                  $('.blurable').removeClass('blur');
-                  $('#cover').removeClass('show_cover');
-                  $('#auth').addClass('faint');
+                  $("#auth-success").show().text(data.success)
+                  $("#auth").html(data.html)
+                  $('.login').addClass('hidden')
+                  $('.blurable').removeClass('blur')
+                  $('#cover').removeClass('show_cover')
+                  $('#auth').addClass('faint')
                 }
               },
               timeout: 60000
-            });
+            })
           }
         },
         timeout: 10000
-      });
+      })
     }
-    return false;
+    return false
   }
 
   function setCheckerTimer(ev) {
-    ev.data.valid = false;
-    checkValidated();
-    window.clearTimeout(ev.data.timer);
-    ev.data.timer = window.setTimeout(ev.data.check, 1100);
+    ev.data.valid = false
+    checkValidated()
+    window.clearTimeout(ev.data.timer)
+    ev.data.timer = window.setTimeout(ev.data.check, 1100)
   }
 
   function checkValidated() {
     if (valid = (fields.user.valid && fields.pass.valid && fields.conf.valid)) {
-      $('#err_msg').text('');
-      $('#s_submit').removeClass('disabled greyed');
+      $('#err_msg').text('')
+      $('#s_submit').removeClass('disabled greyed')
     } else {
-      $('#s_submit').addClass('disabled greyed');
+      $('#s_submit').addClass('disabled greyed')
     }
   }
 
   function checkEmpty() {
     if ($('#s_user').val().length < 1 && $('#s_pass').val().length < 1) {
-      $('#err_msg').text('');
+      $('#err_msg').text('')
     }
   }
 
   function checkUser() {
-    var input, val, self = fields.user;
-    input = $('#s_user');
-    val = input.val();
-    self.valid = false;
+    var input, val, self = fields.user
+    input = $('#s_user')
+    val = input.val()
+    self.valid = false
     if (val === undefined || val === null || val.length < 1) {
-      checkEmpty();
-      input.parent().children(".icon").removeClass("green err").html("&#xF007;");
+      checkEmpty()
+      input.parent().children(".icon").removeClass("green err").html("&#xF007;")
     } else {
-      if (self.ajax) self.ajax.abort();
+      if (self.ajax) self.ajax.abort()
       self.ajax = $.ajax({
         type: "POST",
         url: 'validate',
@@ -121,39 +121,39 @@ $(document).ready(function() {
         data: JSON.stringify({ username: val }),
         dataType: "json",
         error: function(jqXHR, textStatus, errorThrown) {
-          self.valid = false;
-          $('#err_msg').text(textStatus);
-          console.log(textStatus);
-          console.log(jqXHR);
-          input.parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;");
+          self.valid = false
+          $('#err_msg').text(textStatus)
+          console.log(textStatus)
+          console.log(jqXHR)
+          input.parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;")
         },
         success: function(data) {
           if (data.user.valid) {
-            self.valid = data.user.valid;
-            input.parent().children(".icon").removeClass("err").addClass("green").html("&#xF007;");
+            self.valid = data.user.valid
+            input.parent().children(".icon").removeClass("err").addClass("green").html("&#xF007;")
           } else {
-            self.valid = false;
-            if (data.user.err) $('#err_msg').text(data.user.err);
-            input.parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;");
+            self.valid = false
+            if (data.user.err) $('#err_msg').text(data.user.err)
+            input.parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;")
           }
-          checkValidated();
+          checkValidated()
         },
         timeout: 5000
-      });
+      })
     }
   }
 
   function checkPass() {
-    var input, val, self = fields.pass;
-    input = $('#s_pass');
-    val = input.val();
-    self.valid = false;
-    confPass();
+    var input, val, self = fields.pass
+    input = $('#s_pass')
+    val = input.val()
+    self.valid = false
+    confPass()
     if (val === undefined || val === null || val.length < 1) {
-      checkEmpty();
-      input.parent().children(".icon").removeClass("green err").html("&#xF084;");
+      checkEmpty()
+      input.parent().children(".icon").removeClass("green err").html("&#xF084;")
     } else {
-      if (self.ajax) self.ajax.abort();
+      if (self.ajax) self.ajax.abort()
       self.ajax = $.ajax({
         type: "POST",
         url: 'validate',
@@ -161,42 +161,42 @@ $(document).ready(function() {
         data: JSON.stringify({ password: val }),
         dataType: "json",
         error: function(jqXHR, textStatus, errorThrown) {
-          self.valid = false;
-          $('#err_msg').text(textStatus);
-          console.log(textStatus);
-          console.log(jqXHR);
-          input.parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;");
+          self.valid = false
+          $('#err_msg').text(textStatus)
+          console.log(textStatus)
+          console.log(jqXHR)
+          input.parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;")
         },
         success: function(data) {
           if (data.pass.valid) {
-            self.valid = data.pass.valid;
-            input.parent().children(".icon").removeClass("err").addClass("green").html("&#xF084;");
+            self.valid = data.pass.valid
+            input.parent().children(".icon").removeClass("err").addClass("green").html("&#xF084;")
           } else {
-            self.valid = false;
-            if (data.pass.err) $('#err_msg').text(data.pass.err);
-            input.parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;");
+            self.valid = false
+            if (data.pass.err) $('#err_msg').text(data.pass.err)
+            input.parent().children(".icon").removeClass("green").addClass("err").html("&#xF071;")
           }
-          checkValidated();
+          checkValidated()
         },
         timeout: 5000
-      });
+      })
     }
   }
 
   function confPass() {
-    var input, val, self = fields.conf;
-    input = $('#s_pass_conf');
-    val = input.val();
+    var input, val, self = fields.conf
+    input = $('#s_pass_conf')
+    val = input.val()
     if (val === undefined || val === null || val.length < 1) {
-      self.valid = false;
-      input.parent().children(".icon").removeClass("green err").addClass('invert').html("&#xF064;");
+      self.valid = false
+      input.parent().children(".icon").removeClass("green err").addClass('invert').html("&#xF064;")
     } else if (val !== $('#s_pass').val()) {
-      self.valid = false;
-      input.parent().children(".icon").removeClass("green invert").addClass("err").html("&#xF119;");
+      self.valid = false
+      input.parent().children(".icon").removeClass("green invert").addClass("err").html("&#xF119;")
     } else {
-      self.valid = true;
-      input.parent().children(".icon").removeClass("err invert").addClass("green").html("&#xF00C;");
+      self.valid = true
+      input.parent().children(".icon").removeClass("err invert").addClass("green").html("&#xF00C;")
     }
-    checkValidated();
+    checkValidated()
   }
-});
+})
--- src/site.js
+++ src/site.js
@@ -3,113 +3,113 @@ $(document).ready(function() {
     , timeid = -1
     , lastScroll = 0
     , didDrag = false
-    , page = window.location.pathname.slice(1) + window.location.search;
-  if (page === '') { page = 'home'; }
-  page = page.match(/home|about|projects|blog(?!\?title=|\?id=)/);
+    , page = window.location.pathname.slice(1) + window.location.search
+  if (page === '') { page = 'home' }
+  page = page.match(/home|about|projects|blog(?!\?title=|\?id=)/)
   if (page && page.length > 0) {
-    $('#' + page[0]).children().addClass('current-page');
+    $('#' + page[0]).children().addClass('current-page')
   }
   $(window).scroll(function() {
-    ((s = $(window).scrollTop()) > 25 ? s = 25 : s) < 0 ? s = 0 : s;
-    if (s === lastScroll) return;
-    $('header').css('line-height', (45 - s) + 'px');
-    $('header > .line').css('height', (55 - s) + 'px');
-    lastScroll = s;
-  });
+    ((s = $(window).scrollTop()) > 25 ? s = 25 : s) < 0 ? s = 0 : s
+    if (s === lastScroll) return
+    $('header').css('line-height', (45 - s) + 'px')
+    $('header > .line').css('height', (55 - s) + 'px')
+    lastScroll = s
+  })
   window.setTimeout(function() {
-    $('#cover').addClass('cover-transition');
-  }, 1000);
+    $('#cover').addClass('cover-transition')
+  }, 1000)
   function hideLogin(time) {
-    window.clearTimeout(timeid);
+    window.clearTimeout(timeid)
     timeid = window.setTimeout(function() {
-      $('.login').addClass('hidden');
-      $('.blurable').removeClass('blur');
-      $('#cover').removeClass('show_cover');
-      $('#auth').addClass('faint');
-    }, time);
+      $('.login').addClass('hidden')
+      $('.blurable').removeClass('blur')
+      $('#cover').removeClass('show_cover')
+      $('#auth').addClass('faint')
+    }, time)
   }
   function showLogin(time) {
-    window.clearTimeout(timeid);
-    timeid = -1;
-    $('#auth').removeClass('faint');
+    window.clearTimeout(timeid)
+    timeid = -1
+    $('#auth').removeClass('faint')
     if (user === false) {
       timeid = window.setTimeout(function() {
-        $('.login').removeClass('hidden');
-        if (window.innerWidth > 720) $('.blurable').addClass('blur');
-        $('#cover').addClass('show_cover');
-      }, time);
+        $('.login').removeClass('hidden')
+        if (window.innerWidth > 720) $('.blurable').addClass('blur')
+        $('#cover').addClass('show_cover')
+      }, time)
     }
   }
   $('#auth, .login_box, .login > .triangle').hover(function(e) {
-    showLogin(300);
+    showLogin(300)
   }, function(e) {
-    hideLogin(800);
-  });
+    hideLogin(800)
+  })
   $('#auth_touchbox').on('touchstart', function(e) {
-    didDrag = false;
+    didDrag = false
   }).on('touchmove', function(e) {
-    didDrag = true;
+    didDrag = true
   }).on('touchend', function(e) {
-    if (!didDrag) showLogin(60);
-  });
+    if (!didDrag) showLogin(60)
+  })
   $('#cover').on('touchend', function(e) {
-    hideLogin(100);
+    hideLogin(100)
   }).hover(function(e) {
-    if (timeid === -1) hideLogin(800);
-  });
+    if (timeid === -1) hideLogin(800)
+  })
   $(this).on('mouseout', function(e) {
-    var from = e.relatedTarget || e.toElement;
+    var from = e.relatedTarget || e.toElement
     if (!from || from.nodeName == "HTML") {
-        window.clearTimeout(timeid);
-        timeid = -1;
+        window.clearTimeout(timeid)
+        timeid = -1
     }
-  });
+  })
   $('#auth').click(function(e) {
-    if (user === false) return false;
-    $('#ainfo').text('Logging out...');
-    $("#auth-err, #auth-success").hide();
-    $('#auth').addClass('faint');
+    if (user === false) return false
+    $('#ainfo').text('Logging out...')
+    $("#auth-err, #auth-success").hide()
+    $('#auth').addClass('faint')
     $.ajax({
       type: "POST",
       url: '/logout',
       dataType: "html",
       error: function(jqXHR, textStatus, errorThrown) {
-        console.log(textStatus);
+        console.log(textStatus)
       },
       success: function(data) {
-        $("#auth").html(data);
+        $("#auth").html(data)
       },
       timeout: 60000
-    });
-    return false;
-  });
+    })
+    return false
+  })
   $('#login').submit(function(e) {
-    $("#auth-err, #auth-success").hide();
+    $("#auth-err, #auth-success").hide()
     $.ajax({
       type: "POST",
       url: '/auth',
       data: $('#login').serialize(),
       dataType: "json",
       error: function(jqXHR, textStatus, errorThrown) {
-        console.log(textStatus);
+        console.log(textStatus)
       },
       success: function(data) {
         if (data.error) {
-          $("#auth-err").show().text(data.error);
+          $("#auth-err").show().text(data.error)
         } else if (data.success) {
-          $("#auth-success").show().text(data.success);
-          $("#auth").html(data.html);
-          window.clearTimeout(timeid);
+          $("#auth-success").show().text(data.success)
+          $("#auth").html(data.html)
+          window.clearTimeout(timeid)
           timeid = window.setTimeout(function() {
-            $('.login').addClass('hidden');
-            $('.blurable').removeClass('blur');
-            $('#cover').removeClass('show_cover');
-            $('#auth').addClass('faint');
-          }, 2000);
+            $('.login').addClass('hidden')
+            $('.blurable').removeClass('blur')
+            $('#cover').removeClass('show_cover')
+            $('#auth').addClass('faint')
+          }, 2000)
         }
       },
       timeout: 60000
-    });
-    return false;
-  });
-});
+    })
+    return false
+  })
+})
